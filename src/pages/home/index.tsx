54389173b1c5e5aa5769552c2b1084afad41c4ee
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-02-20 19:25:50 +0500

    Fron tugagi

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,16 @@
-// import AboutComponents from "../../components/about";
-// import BookCard from "../../components/bookCard";
-// import Navbar from "../../components/navbar";
-// import Programming from "../../components/programming";
-// import Showcase from "../../components/showcase";
-import img from "../../assets/Screenshot 2025-02-18 171009.png";
+import AboutComponents from "../../components/about";
+import BookCard from "../../components/bookCard";
+import Navbar from "../../components/navbar";
+import Programming from "../../components/programming";
+import Showcase from "../../components/showcase";
 function Home() {
   return (
     <div>
-      <img src={img} alt="" />
-      {/* <Navbar />
+      <Navbar />
       <Showcase />
       <AboutComponents />
       <Programming />
-      <BookCard /> */}
+      <BookCard />
     </div>
   );
 }
